01da74c1a14d49a54d953ce9c7122626514f14cf
AbergerJonas <jonas.aberger@htl-saalfelden.at>
2024-02-09 10:09:35 +0100

    -Added Game-Loop
    -Added Formatter for Python

--- Game/main.py
+++ Game/main.py
@@ -1,1 +1,18 @@
-print("Hello World!")
+import pygame
+pygame.init()
+
+# Define Size of Game-Window
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Main-Game Loop
+run = True
+while run:
+    # Event Handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
