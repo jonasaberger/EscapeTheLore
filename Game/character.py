5a41857dd4dcbc1a9b80c7ef1cd12480e17a8458
AbergerJonas <jonas.aberger@htl-saalfelden.at>
2024-03-01 13:32:54 +0100

    - Character Turning Sprites

--- Game/character.py
+++ Game/character.py
@@ -8,9 +8,31 @@ class Character():
         self.rect = pygame.Rect(0,0,40,40)
         self.rect.center = (x,y)
 
+    
+
+        
+
 
     # Player Movement Function
     def move(self, dx, dy):
+
+        def scale_player(playerImage):
+            image_width = playerImage.get_width()
+            image_height = playerImage.get_height()
+            return pygame.transform.scale(playerImage, (image_width * constants.GAME_SCALE, image_height * constants.GAME_SCALE)) 
+
+
+        if dx < 0:
+            self.image = scale_player((pygame.image.load("Game/assets/images/characters/Player/Idle/Left/0.png").convert_alpha()))
+        if dx > 0:
+            self.image = scale_player((pygame.image.load("Game/assets/images/characters/Player/Idle/Right/0.png").convert_alpha()))
+
+        if dy < 0:
+            self.image = scale_player((pygame.image.load("Game/assets/images/characters/Player/Idle/Up/0.png").convert_alpha()))
+        if dy > 0:
+            self.image = scale_player((pygame.image.load("Game/assets/images/characters/Player/Idle/Down/0.png").convert_alpha()))
+
+
         # Diagonal Speed
         if dx != 0 and dy != 0:
             dx = dx * (math.sqrt(2)/2)
@@ -19,7 +41,11 @@ class Character():
         self.rect.x += dx 
         self.rect.y += dy
 
+        
+
     # Draw the Player Character
     def draw(self, surface):
         surface.blit(self.image,self.rect)
         pygame.draw.rect(surface, constants.RED, self.rect,1)
+
+
